8904a994b6f83a33a7bbef8acca632fa62e68bf4
AMfeta99 <74252797+AMfeta99@users.noreply.github.com>
2020-12-22 14:41:33 +0000

    fft (plot)

--- readFiles_plot_fft.py
+++ readFiles_plot_fft.py
@@ -11,7 +11,8 @@ import os
 import scipy.io
 import random
 import numpy as np
-from scipy.fft import rfft
+from scipy.fft import rfft, rfftfreq
+from matplotlib import pyplot as plt
 
 # Read training set.
 def readTrainFiles():
@@ -58,16 +59,30 @@ def readTrainFiles():
 
 def Preprocessing_fft(zip_data):
     data_EEG, number_channels, labels = zip(*zip_data)
-    data_EEG_fft=[]
+   
+    data_EEG=list(data_EEG)
+    N=5000*600
+    SAMPLE_RATE=5000
+    xf = rfftfreq(N, 1 / SAMPLE_RATE)
     
     #pre-processing using FFT (fast Fourier transform)
     for j in range (len(data_EEG)):
-        yf = rfft(data_EEG[j])
-        data_EEG_fft.append(yf)
+        #for each signal
         
-    train = list(zip(data_EEG_fft, number_channels, labels))
-    return train
-    
+        channel=data_EEG[j] 
+        data_channels_fft=[]
+        for i in range (len(channel)):
+            #for each channel
+            yf = rfft(channel[i])
+            # plt.plot(xf, np.abs(yf))
+            # plt.show()
+            data_channels_fft.append(yf)
+
+        data_EEG[j]= np.array((data_channels_fft))
+        
+    data_EEG=tuple(data_EEG)
+    train2 = list(zip(data_EEG, number_channels, labels))
+    return train2
     
 
 train=readTrainFiles()
